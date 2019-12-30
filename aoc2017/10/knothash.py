7ce28c81f4262a8f1ec0e650c07938e4ed1e2473
sdfrew2 <sdfrew2@users.noreply.github.com>
2019-12-30 19:21:09 +0100

    Day 10 part 2

--- aoc2017/10/knothash.py
+++ aoc2017/10/knothash.py
@@ -3,20 +3,30 @@
 
 
 
-def knothash(bytes):
+def knothash(s):
     buffer = list(range(256))
     skip = 0
     pos = 0
-    for l in bytes:
-        hl = l // 2
-        for i in range(hl):
-            left = (pos + i) % 256
-            right = (pos + l - 1 - i) % 256
-            buffer[left], buffer[right] = buffer[right], buffer[left]
-        pos += l + skip
-        skip += 1
-    return buffer
+    bytes = [ord(c) for c in s]
+    bytes += [17, 31, 73, 47, 23]
+    for round in range(64):
+        for l in bytes:
+            hl = l // 2
+            for i in range(hl):
+                left = (pos + i) % 256
+                right = (pos + l - 1 - i) % 256
+                buffer[left], buffer[right] = buffer[right], buffer[left]
+            pos += l + skip
+            skip += 1
+    hexparts = []
+    for i in range(16):
+        xsum = 0
+        for j in range(16):
+            xsum ^= buffer[16 * i + j]
+        hexpart = hex(xsum)[2:].rjust(2, "0")
+        hexparts.append(hexpart)
+    return "".join(hexparts)
 
-INPUT = [int(s) for s in open("input.txt", "r").read().strip().split(",")]
+INPUT =open("input.txt", "r").read().strip()
 h = knothash(INPUT)
-print(h[0] * h[1])
+print(h)
